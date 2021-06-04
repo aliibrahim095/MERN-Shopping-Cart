64469d360d5cf06c97f5a7ad36cf94a5c9731401
aliibrahim095 <4liebrahim@gmail.com>
2021-06-05 00:27:01 +0200

    productscreen content and design, make it responsive

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,50 @@
-import './ProductScreen.css'
+import "./ProductScreen.css";
 
 export const ProductScreen = () => {
-    return (
-        <div className="productscreen">
-            Product Screen
+  return (
+    <div className="productscreen">
+      <div className="productscreen_left">
+        <div className="left__image">
+          <img
+            src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"
+            alt="product name"
+          />
         </div>
-    )
-}
+        <div className="left__info">
+            <p className="left__name">PlayStation</p>
+            <p>Price: $499.99</p>
+            <p>Description: Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum Lorem ipsum .
+            </p>
+        </div>
+      </div>
+
+        <div className="productscreen_right">
+            <div className="right__info">
+                <p>
+                    Price: <span>$499.99</span>
+                </p>
+                <p>
+                    Status: <span>In Stock</span>
+                </p>
+                <p>
+                    Qty
+                    <select>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                    </select>
+                </p>
+                <p>
+                    <button type="button">Add To Cart</button>
+                </p>
+            </div>
+        
+        </div>
+    
+    </div>
+  );
+};
 
 export default ProductScreen;
