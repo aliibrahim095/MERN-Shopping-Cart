f780543c1c6efa69a454bab192f36614c659987e
aliibrahim095 <4liebrahim@gmail.com>
2021-06-05 12:50:58 +0200

    adding Cart actions, constants, reducers

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,7 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-const reducer = combineReducers({});
+
+import {cartReducer} from './reducers/cartReducers'
+const reducer = combineReducers({
+    cart:cartReducer
+});
 const middleware = [thunk]; //helps us to make asynchronus requests in our actions
 
 const store = createStore(
