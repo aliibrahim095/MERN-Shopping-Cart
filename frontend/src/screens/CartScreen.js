4c8dd0a87982d5a52c6925e78c842437186283db
aliibrahim095 <4liebrahim@gmail.com>
2021-06-05 10:48:28 +0200

    CartScreen content and designing it, make it responsive

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,9 +1,26 @@
 import './CartScreen.css'
-
+import CartItem from '../components/CartItem'
 export const CartScreen = () => {
     return (
         <div className="cartscreen">
-            Cart Screen
+            <div className="cartscreen__left">
+                <h2>shopping Cart</h2>
+                <CartItem/>
+                <CartItem/>
+                <CartItem/>
+                <CartItem/>
+                <CartItem/>
+            </div>
+            <div className="cartscreen__right">
+                <div className="cartscreen__info">
+                    <p>Subtotal (0) items</p>
+                    <p>$499.99</p>
+                </div>
+                <div>
+                    <button>Proceed To Checkout</button>
+                </div>
+
+            </div>
         </div>
     )
 }
